34285df957cdc484cbfb82d6c5b01573546677ab
Will Carter <will.carter@fergusllc.com>
2020-01-18 23:10:43 +0000

    Automatically backed up by Learn

--- lib/main.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'board.rb'
-require_relative 'game.rb'
-require_relative 'player.rb'
-require_relative 'players/computer.rb'
-require_relative 'players/human.rb'
-
-game = Game.new
-game.play
